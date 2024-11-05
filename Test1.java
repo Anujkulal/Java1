d8ad0996cf2a59f7b36d90c38dbfbd3456f8b78a
anujkulal <anujkulal333@gmail.com>
2024-11-05 21:38:20 +0530

    1

--- Test1.java
+++ Test1.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Test1 {
-    public static void calculator(int a, int b, String op) {
+    public static void calculator(double a, double b, String op) {
         double result;
         switch (op) {
             case "+":
@@ -22,7 +22,7 @@ public class Test1 {
                 if (b == 0)
                     System.out.println("Cannot divide by zero");
                 else {
-                    result = a / b;
+                    result = a/b;
                     System.out.println(a + " / " + b + " = " + result);
                 }
                 break;
@@ -35,8 +35,8 @@ public class Test1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the two number: ");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        double num1 = sc.nextInt();
+        double num2 = sc.nextInt();
         System.out.print("Enter the operator: ");
         String op = sc.next();
         calculator(num1, num2, op);
